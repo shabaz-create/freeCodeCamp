9c36a585432e25f92b843e832e56639466b0ca65
Shabaz Khan <73603013+shabaz-create@users.noreply.github.com>
2022-02-04 19:47:37 +0530

    code
    Code for Telephone number Validator

---
+++ javascript_datastructures_and_algorithms/telephone-number-validator/telephone-number-validator.js
@@ -0,0 +1,13 @@
+function telephoneCheck(str) {
+  //the regex is quite complex if we check case by case then it will be easy
+  //be careful while reading the regex it might be confusing
+ let regex=/^1\s?([0-9]{3}\s){2}[0-9]{4}$|^1\s?\([0-9]{3}\)\s[0-9]{3}-[0-9]{4}$|^1?\s(([0-9]{3}-)){2}[0-9]{4}$|^([0-9]{3}-){2}[0-9]{4}$|^1?[0-9]{10}$|^1?\([0-9]{3}\)[0-9]{3}-[0-9]{4}/
+
+//simple if else statemnt to validate the number using given regex 
+ if(regex.test(str)){
+   return true;
+ }else{
+   console.log(false);
+   return false;
+ }
+}
