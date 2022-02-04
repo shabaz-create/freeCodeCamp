f644dc66784f945b3efdbd82fb093096d9bbebc4
Shabaz Khan <73603013+shabaz-create@users.noreply.github.com>
2022-02-04 19:58:16 +0530

    code
    Removed redauntant code

--- javascript_datastructures_and_algorithms/telephone-number-validator/telephone-number-validator.js
+++ javascript_datastructures_and_algorithms/telephone-number-validator/telephone-number-validator.js
@@ -1,7 +1,7 @@
 function telephoneCheck(str) {
   //the regex is quite complex if we check case by case then it will be easy
   //be careful while reading the regex it might be confusing
- let regex=/^1\s?([0-9]{3}\s){2}[0-9]{4}$|^1\s?\([0-9]{3}\)\s[0-9]{3}-[0-9]{4}$|^1?\s(([0-9]{3}-)){2}[0-9]{4}$|^([0-9]{3}-){2}[0-9]{4}$|^1?[0-9]{10}$|^1?\([0-9]{3}\)[0-9]{3}-[0-9]{4}/
+ let regex=/^(1?\s?)(([0-9]{3}\s){2}|\([0-9]{3}\)\s[0-9]{3}-|(([0-9]{3}-)){2}|^([0-9]{3}-){2}|[0-9]{6}|\([0-9]{3}\)[0-9]{3}-)[0-9]{4}$/
 
 //simple if else statemnt to validate the number using given regex 
  if(regex.test(str)){
